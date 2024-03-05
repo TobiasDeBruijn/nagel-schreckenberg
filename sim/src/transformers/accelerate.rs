dbe7787768cd534d093c9fcd0cc1981b49a1926c
Tobias de Bruijn <t.debruijn@array21.dev>
2024-03-05 18:15:54 +0100

    WIP accelerate

--- sim/src/transformers/accelerate.rs
+++ sim/src/transformers/accelerate.rs
@@ -1,11 +1,35 @@
 use crate::transformers::Transformer;
-use crate::typedef::Road;
+use crate::typedef::{Road, Velocity};
 
 pub struct Accelerator;
 
 impl Transformer<Road> for Accelerator {
     fn transform(self, r: Road) -> Road {
-        // TODO
-        r
+        for lane in 0..=3_u8 {
+            r = apply_lane(r)
+        }
     }
+}
+
+fn apply_lane(r: &mut Road, lane: u8) {
+    let vmax = r.get_max_velocity_in_lane(lane).expect("Getting vmax for a lane");
+    let mut vs = r.get_vehicles_in_lane_mut(lane);
+
+    let to_accelerate = vs
+        .windows(2)
+        .into_iter()
+        .filter(|vs| {
+            let v = &vs[0];
+            match vs.get(1) {
+                Some(vnext) => v.velocity < vmax && v.position.distance_1d(&vnext.position) > (v.velocity.into_inner() + 1) as u128,
+                None => v.velocity < vmax
+            }
+        })
+        .map(|f| f[0].position.clone())
+        .collect::<Vec<_>>();
+
+    vs
+        .iter_mut()
+        .filter(|f| to_accelerate.contains(&f.position))
+        .for_each(|f| f.velocity += Velocity::new(1));
 }
--- sim/src/typedef.rs
+++ sim/src/typedef.rs
@@ -1,25 +1,66 @@
+use std::ops::{Add, AddAssign, Deref, Sub};
+
 pub struct Road {
     vehicles: Vec<Vehicle>,
     speed_per_lane: Vec<Velocity>,
 }
 
 pub struct Vehicle {
-    position: Position,
-    velocity: Velocity,
+    pub position: Position,
+    pub velocity: Velocity,
 }
 
+#[derive(Clone, PartialEq)]
 pub struct Position {
-    x: u128,
-    y: u8,
+    pub x: u128,
+    pub y: u8,
 }
 
-#[derive(Clone, Copy)]
+
+impl Position {
+    pub fn distance_1d(&self, rhs: &Self) -> u128 {
+        self.x - rhs.x
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Velocity(u8);
+
+impl AddAssign for Velocity {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 += rhs.0
+    }
+}
+
+impl Velocity {
+    pub fn new(v: u8) -> Self {
+        Self(v)
+    }
+
+    pub fn into_inner(self) -> u8 {
+        self.0
+    }
+}
+
+impl Deref for Velocity {
+    type Target = u8;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 impl Road {
     pub fn get_vehicles_in_lane(&self, lane: u8) -> Vec<&Vehicle> {
         self.vehicles
             .iter()
+            .filter(|vehicle| vehicle.position.y == lane)
+            .collect::<Vec<_>>()
+    }
+
+    pub fn get_vehicles_in_lane_mut(&mut self, lane: u8) -> Vec<&mut Vehicle> {
+        self.vehicles
+            .iter_mut()
             .filter(|vehicle| vehicle.position.y == lane)
             .collect::<Vec<_>>()
     }
