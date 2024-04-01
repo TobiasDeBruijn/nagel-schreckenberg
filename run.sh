7a016c758947f6e0d7b8fb2c250071b1c33809e4
Geert Haans <geert.haans@gmail.com>
2024-04-01 23:49:08 +0200

    Quick fix

--- run.sh
+++ run.sh
@@ -5,7 +5,11 @@ run_with() {
 	cargo r --release -- -s 50 -i 200 --parameter-under-test density --l1 $1 --l2 $2 --l3 $3 --p-decel 0.4 --p-lane-change $4 -v -o "probabilities/$4/$1$2$3"
 }
 
-new_run_with 5 5 5 0.8
-new_run_with 4 5 5 0.8
-new_run_with 3 5 5 0.8
-new_run_with 2 5 5 0.8
+run_with 5 5 5 0.4
+run_with 4 5 5 0.4
+run_with 3 5 5 0.4
+run_with 2 5 5 0.4
+run_with 5 5 5 0.6
+run_with 4 5 5 0.6
+run_with 3 5 5 0.6
+run_with 2 5 5 0.6
