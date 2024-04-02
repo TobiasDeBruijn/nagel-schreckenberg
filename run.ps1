42a9a52a167fbf30ad27a89da1e7b18f43d2da6e
Geert Haans <geert.haans@gmail.com>
2024-04-02 18:21:17 +0200

    Change data dir structure

--- run.ps1
+++ run.ps1
@@ -4,18 +4,12 @@ function run_with {
         [int]$l1,
         [int]$l2,
         [int]$l3,
+        [double]$p_decel,
         [double]$p_lane_change
     )
 
-    cargo r --release -- -s 50 -i 200 --parameter-under-test density --l1 $l1 --l2 $l2 --l3 $l3 --p-decel 0.4 --p-lane-change $p_lane_change -v -o "probabilities/$p_lane_change/$l1$l2$l3"
+    cargo r --release -- -s 50 -i 200 --parameter-under-test density --l1 $l1 --l2 $l2 --l3 $l3 --p-decel $p_decel --p-lane-change $p_lane_change -v -o "probabilities/$p_lane_change/$l1$l2$l3"
 }
 
 # Run the cargo command with different parameters
-run_with 5 5 5 0.4
-run_with 4 5 5 0.4
-run_with 3 5 5 0.4
-run_with 2 5 5 0.4
-run_with 5 5 5 0.6
-run_with 4 5 5 0.6
-run_with 3 5 5 0.6
-run_with 2 5 5 0.6
+run_with 5 3 5 0.6 1.0
